d557c18e88ccaef2bb166fcf6ab59deb1e73ac7a
Aluno24845 <151393863+Aluno24845@users.noreply.github.com>
2024-06-11 17:43:55 +0100

    Criação da Base de Dados

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,13 +1,27 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using HOTEL360___Trabalho_final.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace HOTEL360___Trabalho_final.Data
-{
+namespace HOTEL360___Trabalho_final.Data{
+
+    /// <summary>
+    /// classe responsável pela criação e gestão da Base de Dados
+    /// </summary>
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
+            : base(options){
         }
+
+        // definição das 'tabelas'
+        public DbSet<Utilizadores> Utilizadores { get; set; }
+        public DbSet<Hospedes> Hospedes { get; set; }
+        public DbSet<Gerentes> Gerentes { get; set; }
+        public DbSet<Reccecionistas> Reccecionistas { get; set; }
+        public DbSet<Quartos> Quartos { get; set; }
+        public DbSet<Reservas_Servicos> ReservasServicos { get; set; }
+        public DbSet<Reservas> Reservas { get; set; }
+        public DbSet<Servicos> Servicos { get; set; }
+
     }
 }
