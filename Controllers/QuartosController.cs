8cfd9fd2cf7ca7390e9f00d3c826b590949bfd44
Aluno24845 <151393863+Aluno24845@users.noreply.github.com>
2024-06-12 13:16:09 +0100

    Em resultado do FileUpload, guardar uma imagem no disco rígido

--- Controllers/QuartosController.cs
+++ Controllers/QuartosController.cs
@@ -11,10 +11,22 @@ using HOTEL360___Trabalho_final.Models;
 namespace HOTEL360___Trabalho_final.Controllers{
     public class QuartosController : Controller {
 
+        /// <summary>
+        /// referência à BD do projeto
+        /// </summary>
         private readonly ApplicationDbContext _context;
 
-        public QuartosController(ApplicationDbContext context) {
+        /// <summary>
+        /// objeto que contém os dados referentes ao ambiente 
+        /// do Servidor
+        /// </summary>
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public QuartosController(
+         ApplicationDbContext context,
+         IWebHostEnvironment webHostEnvironment) {
             _context = context;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         // GET: Quartos
@@ -51,7 +63,65 @@ namespace HOTEL360___Trabalho_final.Controllers{
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Nome,Capacidade,Preco,Descricao,Imagem")] Quartos quarto, IFormFile ImagemLogo) {
-            
+
+            /* Algoritmo
+          * 1- há ficheiro?
+          *    1.1 - não há ficheiro!
+          *          devolver à view dizendo que o ficheiro
+          *          é obrigatório
+          *    1.2 - há ficheiro!
+          *          Mas, é uma imagem (PNG, JPG)?
+          *          1.2.1 - não é PNG nem JPG
+          *                  devolver o controlo à View
+          *                  e pedir PNG ou JPG
+          *          1.2.2 - é uma imagem
+          *                  - determinar o nome a atribuir 
+          *                    ao ficheiro
+          *                  - escrever esse nome na BD
+          *                  - se a escrita na BD se concretizar
+          *                    é que o ficheiro é guardado no 
+          *                    disco rígido
+          */
+
+            // vars. auxiliares
+            string nomeImagem = "";
+            bool haImagem = false;
+
+            // há ficheiro?
+            if (ImagemLogo == null)
+            {
+                ModelState.AddModelError("",
+                   "O fornecimento de uma Imagem é obrigatório!");
+                return View(quarto);
+            }
+            else
+            {
+                // há ficheiro, mas é imagem?
+                if (!(ImagemLogo.ContentType == "image/png" ||
+                       ImagemLogo.ContentType == "image/jpeg")
+                   )
+                {
+                    ModelState.AddModelError("",
+                   "Tem de fornecer para a Imagem um ficheiro PNG ou JPG!");
+                    return View(quarto);
+                }
+                else
+                {
+                    // há ficheiro, e é uma imagem válida
+                    haImagem = true;
+                    // obter o nome a atribuir à imagem
+                    Guid g = Guid.NewGuid();
+                    nomeImagem = g.ToString();
+                    // obter a extensão do nome do ficheiro
+                    string extensao = Path.GetExtension(ImagemLogo.FileName);
+                    // adicionar a extensão ao nome da imagem
+                    nomeImagem += extensao;
+                    // adicionar o nome do ficheiro ao objeto que
+                    // vem do browser
+                    quarto.Imagem = nomeImagem;
+                }
+            }
+
             //avalia se os dados que chegam da View estão de acordo com o Model
             if (ModelState.IsValid) {
 
@@ -59,6 +129,35 @@ namespace HOTEL360___Trabalho_final.Controllers{
                 _context.Add(quarto);
                 //efetua COMMIT na BD
                 await _context.SaveChangesAsync();
+
+                // se há ficheiro de imagem,
+                // vamos guardar no disco rígido do servidor
+                if (haImagem)
+                {
+                    // determinar onde se vai guardar a imagem
+                    string nomePastaOndeGuardarImagem =
+                       _webHostEnvironment.WebRootPath;
+                    // já sei o caminho até à pasta wwwroot
+                    // especifico onde vou guardar a imagem
+                    nomePastaOndeGuardarImagem =
+                       Path.Combine(nomePastaOndeGuardarImagem, "Imagens");
+                    // e, existe a pasta 'Imagens'?
+                    if (!Directory.Exists(nomePastaOndeGuardarImagem))
+                    {
+                        Directory.CreateDirectory(nomePastaOndeGuardarImagem);
+                    }
+                    // juntar o nome do ficheiro à sua localização
+                    string nomeFinalDaImagem =
+                       Path.Combine(nomePastaOndeGuardarImagem, nomeImagem);
+
+                    // guardar a imagem no disco rigído
+                    using var stream = new FileStream(
+                       nomeFinalDaImagem, FileMode.Create
+                       );
+                    await ImagemLogo.CopyToAsync(stream);
+                }
+
+
                 //redireciona o utilizador para a página Index 
                 return RedirectToAction(nameof(Index));
             }
