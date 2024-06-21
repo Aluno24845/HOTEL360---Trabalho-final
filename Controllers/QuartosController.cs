d0751cfbed69f05093264e4fd27613e66e69145f
Aluno24845 <151393863+Aluno24845@users.noreply.github.com>
2024-06-21 17:27:03 +0100

    Alterações na função de editar e apagar um quarto

--- Controllers/QuartosController.cs
+++ Controllers/QuartosController.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using HOTEL360___Trabalho_final.Data;
 using HOTEL360___Trabalho_final.Models;
 using Microsoft.AspNetCore.Authorization;
+using System.Drawing;
 
 namespace HOTEL360___Trabalho_final.Controllers{
 
@@ -199,11 +200,17 @@ namespace HOTEL360___Trabalho_final.Controllers{
                 return NotFound();
             }
 
-            var quartos = await _context.Quartos.FindAsync(id);
-            if (quartos == null) {
+            var quarto = await _context.Quartos.FindAsync(id);
+            if (quarto == null) {
                 return NotFound();
             }
-            return View(quartos);
+
+            // Preencher o valor de PrecoAux para exibição
+            quarto.PrecoAux = quarto.Preco.ToString();
+
+            string nomeImagem = quarto.Imagem;
+
+            return View(quarto);
         }
 
         // POST: Quartos/Edit/5
@@ -211,27 +218,119 @@ namespace HOTEL360___Trabalho_final.Controllers{
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Capacidade,Preco,Descricao,Imagem")] Quartos quartos) {
-            if (id != quartos.Id)  {
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Capacidade,Preco,PrecoAux,Descricao,Imagem")] Quartos quarto, IFormFile ImagemLogo) {
+            if (id != quarto.Id)  {
                 return NotFound();
             }
+            /* Algoritmo
+              * 1- há ficheiro?
+              *    1.1 - não há ficheiro!
+              *          devolver à view dizendo que o ficheiro
+              *          é obrigatório
+              *    1.2 - há ficheiro!
+              *          Mas, é uma imagem (PNG, JPG)?
+              *          1.2.1 - não é PNG nem JPG
+              *                  devolver o controlo à View
+              *                  e pedir PNG ou JPG
+              *          1.2.2 - é uma imagem
+              *                  - determinar o nome a atribuir 
+              *                    ao ficheiro
+              *                  - escrever esse nome na BD
+              *                  - se a escrita na BD se concretizar
+              *                    é que o ficheiro é guardado no 
+              *                    disco rígido
+              */
 
+            // vars. auxiliares
+            string nomeImagem = "";
+            bool haImagem = false;
+
+            // há ficheiro?
+            if (ImagemLogo == null)
+            {
+                ModelState.AddModelError("",
+                   "O fornecimento de uma Imagem é obrigatório!");
+                return View(quarto);
+            }
+            else
+            {
+                // há ficheiro, mas é imagem?
+                if (!(ImagemLogo.ContentType == "image/png" ||
+                       ImagemLogo.ContentType == "image/jpeg")
+                   )
+                {
+                    ModelState.AddModelError("",
+                   "Tem de fornecer para a Imagem um ficheiro PNG ou JPG!");
+                    return View(quarto);
+                }
+                else
+                {
+                    // há ficheiro, e é uma imagem válida
+                    haImagem = true;
+                    // obter o nome a atribuir à imagem
+                    Guid g = Guid.NewGuid();
+                    nomeImagem = g.ToString();
+                    // obter a extensão do nome do ficheiro
+                    string extensao = Path.GetExtension(ImagemLogo.FileName);
+                    // adicionar a extensão ao nome da imagem
+                    nomeImagem += extensao;
+                    // adicionar o nome do ficheiro ao objeto que
+                    // vem do browser
+                    quarto.Imagem = nomeImagem;
+                }
+            }
+
+            //avalia se os dados que chegam da View estão de acordo com o Model
             if (ModelState.IsValid) {
                 try {
-                    _context.Update(quartos);
+
+                    //transferir o valor de PrecoAux para Preco
+                    quarto.Preco = Convert.ToDecimal(quarto.PrecoAux.Replace('.', ','));
+
+                    //atualiza os dados vindos da View à BD 
+                    _context.Update(quarto);
+                    //efetua COMMIT na BD
                     await _context.SaveChangesAsync();
+
+                    // se há ficheiro de imagem,
+                    // vamos guardar no disco rígido do servidor
+                    if (haImagem)
+                    {
+                        // determinar onde se vai guardar a imagem
+                        string nomePastaOndeGuardarImagem =
+                           _webHostEnvironment.WebRootPath;
+                        // já sei o caminho até à pasta wwwroot
+                        // especifico onde vou guardar a imagem
+                        nomePastaOndeGuardarImagem =
+                           Path.Combine(nomePastaOndeGuardarImagem, "Imagens");
+                        // e, existe a pasta 'Imagens'?
+                        if (!Directory.Exists(nomePastaOndeGuardarImagem))
+                        {
+                            Directory.CreateDirectory(nomePastaOndeGuardarImagem);
+                        }
+                        // juntar o nome do ficheiro à sua localização
+                        string nomeFinalDaImagem =
+                           Path.Combine(nomePastaOndeGuardarImagem, nomeImagem);
+
+                        // guardar a imagem no disco rigído
+                        using var stream = new FileStream(
+                           nomeFinalDaImagem, FileMode.Create
+                           );
+                        await ImagemLogo.CopyToAsync(stream);
+                    }
+                    //redireciona o utilizador para a página Index 
+                    return RedirectToAction(nameof(Index));
                 }
                 catch (DbUpdateConcurrencyException)   {
-                    if (!QuartosExists(quartos.Id)) {
+                    if (!QuartosExists(quarto.Id)) {
                         return NotFound();
                     }
                     else   {
                         throw;
                     }
                 }
-                return RedirectToAction(nameof(Index));
             }
-            return View(quartos);
+            return View(quarto);
         }
 
         // GET: Quartos/Delete/5
