d65ebc99641f1215b9f610fdc02e2f19ca1f4d8b
Aluno24845 <151393863+Aluno24845@users.noreply.github.com>
2024-07-06 17:04:32 +0100

    Correção da imagem no editar Quarto

--- Controllers/QuartosController.cs
+++ Controllers/QuartosController.cs
@@ -227,68 +227,40 @@ namespace HOTEL360___Trabalho_final.Controllers{
         public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Capacidade,Preco,PrecoAux,Descricao,Imagem, Localizacao")] Quartos quarto, IFormFile ImagemLogo) {
             if (id != quarto.Id)  {
                 return NotFound();
-            }
-            /* Algoritmo
-              * 1- há ficheiro?
-              *    1.1 - não há ficheiro!
-              *          devolver à view dizendo que o ficheiro
-              *          é obrigatório
-              *    1.2 - há ficheiro!
-              *          Mas, é uma imagem (PNG, JPG)?
-              *          1.2.1 - não é PNG nem JPG
-              *                  devolver o controlo à View
-              *                  e pedir PNG ou JPG
-              *          1.2.2 - é uma imagem
-              *                  - determinar o nome a atribuir 
-              *                    ao ficheiro
-              *                  - escrever esse nome na BD
-              *                  - se a escrita na BD se concretizar
-              *                    é que o ficheiro é guardado no 
-              *                    disco rígido
-              */
-
-            // vars. auxiliares
-            string nomeImagem = "";
-            bool haImagem = false;
-
-            // há ficheiro?
-            if (ImagemLogo == null)
-            {
-                ModelState.AddModelError("",
-                   "O fornecimento de uma Imagem é obrigatório!");
-                return View(quarto);
-            }
-            else
-            {
-                // há ficheiro, mas é imagem?
-                if (!(ImagemLogo.ContentType == "image/png" ||
-                       ImagemLogo.ContentType == "image/jpeg")
-                   )
-                {
-                    ModelState.AddModelError("",
-                   "Tem de fornecer para a Imagem um ficheiro PNG ou JPG!");
-                    return View(quarto);
-                }
-                else
-                {
-                    // há ficheiro, e é uma imagem válida
-                    haImagem = true;
-                    // obter o nome a atribuir à imagem
-                    Guid g = Guid.NewGuid();
-                    nomeImagem = g.ToString();
-                    // obter a extensão do nome do ficheiro
-                    string extensao = Path.GetExtension(ImagemLogo.FileName);
-                    // adicionar a extensão ao nome da imagem
-                    nomeImagem += extensao;
-                    // adicionar o nome do ficheiro ao objeto que
-                    // vem do browser
-                    quarto.Imagem = nomeImagem;
-                }
-            }
+            }            
 
             //avalia se os dados que chegam da View estão de acordo com o Model
             if (ModelState.IsValid) {
                 try {
+                    if (ImagemLogo != null && (ImagemLogo.ContentType == "image/png" || ImagemLogo.ContentType == "image/jpeg"))
+                    {
+                        // Obter o nome a atribuir à imagem
+                        Guid g = Guid.NewGuid();
+                        string nomeImagem = g.ToString();
+                        // Obter a extensão do nome da imagem
+                        string extensao = Path.GetExtension(ImagemLogo.FileName);
+                        nomeImagem += extensao;
+
+                        // Define o caminho para guardar a imagem
+                        string nomePastaOndeGuardarImagem = Path.Combine(_webHostEnvironment.WebRootPath, "Imagens");
+                        if (!Directory.Exists(nomePastaOndeGuardarImagem))
+                        {
+                            Directory.CreateDirectory(nomePastaOndeGuardarImagem);
+                        }
+                        string nomeFinalDaImagem = Path.Combine(nomePastaOndeGuardarImagem, nomeImagem);
+
+                        // Guarda a imagem
+                        using var stream = new FileStream(nomeFinalDaImagem, FileMode.Create);
+                        await ImagemLogo.CopyToAsync(stream);
+
+                        // Atualiza o avatar do utilizador
+                        quarto.Imagem = nomeImagem;
+                    }
+                    else
+                    {
+                        // Não atualize o campo Avatar se nenhuma nova imagem for carregada
+                        _context.Entry(quarto).Property(u => u.Imagem).IsModified = false;
+                    }
 
                     //transferir o valor de PrecoAux para Preco
                     quarto.Preco = Convert.ToDecimal(quarto.PrecoAux.Replace('.', ','));
@@ -298,32 +270,7 @@ namespace HOTEL360___Trabalho_final.Controllers{
                     //efetua COMMIT na BD
                     await _context.SaveChangesAsync();
 
-                    // se há ficheiro de imagem,
-                    // vamos guardar no disco rígido do servidor
-                    if (haImagem)
-                    {
-                        // determinar onde se vai guardar a imagem
-                        string nomePastaOndeGuardarImagem =
-                           _webHostEnvironment.WebRootPath;
-                        // já sei o caminho até à pasta wwwroot
-                        // especifico onde vou guardar a imagem
-                        nomePastaOndeGuardarImagem =
-                           Path.Combine(nomePastaOndeGuardarImagem, "Imagens");
-                        // e, existe a pasta 'Imagens'?
-                        if (!Directory.Exists(nomePastaOndeGuardarImagem))
-                        {
-                            Directory.CreateDirectory(nomePastaOndeGuardarImagem);
-                        }
-                        // juntar o nome do ficheiro à sua localização
-                        string nomeFinalDaImagem =
-                           Path.Combine(nomePastaOndeGuardarImagem, nomeImagem);
-
-                        // guardar a imagem no disco rigído
-                        using var stream = new FileStream(
-                           nomeFinalDaImagem, FileMode.Create
-                           );
-                        await ImagemLogo.CopyToAsync(stream);
-                    }
+                    
                     //redireciona o utilizador para a página Index 
                     return RedirectToAction(nameof(Index));
                 }
