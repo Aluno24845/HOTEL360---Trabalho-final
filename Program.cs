3e14cc60443c1c4068df9dd4282ec8ff1ef6e771
Aluno24845 <151393863+Aluno24845@users.noreply.github.com>
2024-07-10 19:01:44 +0100

    Alteração no ficheiro Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 using HOTEL360___Trabalho_final.Data;
+using HOTEL360___Trabalho_final.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,15 +16,34 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-
+builder.Services.AddAntiforgery(options => { options.SuppressXFrameOptionsHeader = true; });
+builder.Services.AddControllers().AddJsonOptions(x =>
+   x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 // referência ao serviço do Microsoft Identity
 // que faz a AUTENTICAÇÃO 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddRoles<IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<IdentityUser>(opt =>
+{
+    opt.SignIn.RequireConfirmedAccount = true;
+})
+     .AddRoles<IdentityRole>()
+     .AddDefaultUI()
+     .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.ConfigureApplicationCookie(options => { options.Cookie.SameSite = SameSiteMode.None; });
+
 // a referência ao IdentityRole inicia o uso desse tipo de autorização 
 builder.Services.AddControllersWithViews();
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(MyAllowSpecificOrigins,
+                          policy =>
+                          {
+                              policy.WithOrigins("http://localhost:3000")
+                                                  .AllowAnyHeader()
+                                                  .AllowAnyMethod();
+                          });
+});
 
 var app = builder.Build();
 
@@ -36,12 +58,12 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseCors(MyAllowSpecificOrigins);
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.MapIdentityApi<IdentityUser>();
 app.UseAuthorization();
 
 app.MapControllerRoute(
